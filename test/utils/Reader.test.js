535b51dc8403aade46ca8d2b825ba32d61c9e44e
David Patiño <david.andrespr5@gmail.com>
2022-05-10 20:12:10 -0500

    9. Arreglando archivos con ESLint

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,7 +1,7 @@
-const Reader = require('./../../lib/utils/Reader')
-describe('Utils Test',()=>{
-    test('Class Reader',()=>{
-        const Read = Reader.readJsonFile("students.json")
+const Reader = require("./../../lib/utils/Reader");
+describe("Utils Test",()=>{
+    test("Class Reader",()=>{
+        const Read = Reader.readJsonFile("students.json");
         expect(Read).not.toBeUndefined();
-    })
-})
+    });
+});
