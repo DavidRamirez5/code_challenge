790f6bf9121bc2c29b11a7df2f02a4db1daaab9f
David Patiño <david.andrespr5@gmail.com>
2022-05-18 16:56:01 -0500

    Agregando pruebas de unidad de metodo getEmailWithCertification()

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -6,4 +6,11 @@ describe("Class StudentService",()=>{
         const getStudents = StudentService.getStudents(students);
         expect(getStudents).not.toBeUndefined();
     });
+    test("Metodo getEmailWithCertification",()=>{
+        const students = [{"email": "Todd@visualpartnership.xyz","haveCertification": false},{"email": "Sexton@visualpartnership.xyz","haveCertification": true},{"email": "Sharlene@visualpartnership.xyz","haveCertification": true}]
+        const certification = StudentService.getEmailsWithCertification(students)
+        expect(certification).toEqual(["Sexton@visualpartnership.xyz", "Sharlene@visualpartnership.xyz"]);
+        expect(certification).not.toBeUndefined();
+        
+    })
 });
