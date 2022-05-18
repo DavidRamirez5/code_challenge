33043206ebbf2c1ad0de9a0017192f89a50b985c
David Patiño <david.andrespr5@gmail.com>
2022-05-18 18:04:33 -0500

    Agregando pruebas de unidad de Metodo studentCredits de class StudentController

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -5,8 +5,14 @@ describe("Class StudentController",()=> {
         const Students = StudentController.getStudents();
         expect(Students).not.toBeUndefined();
     });
-    test("1.Metodo getEmailWithCertification",()=>{
+    test("2.Metodo getEmailWithCertification",()=>{
         const haveCertification = StudentController.getEmailwithCertification();
         expect(haveCertification).not.toBeUndefined();
+    })
+    test("3. Metodo studentCredits",()=>{
+        const credits = StudentController.studentCredits(500);
+        expect(credits).not.toBeUndefined();
+        expect(credits.length).toBe(27);
+
     })
 });
