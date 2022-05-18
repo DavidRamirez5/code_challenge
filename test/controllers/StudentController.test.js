7930d722a609db9e94236d5e4ff41fb826f457c8
David Patiño <david.andrespr5@gmail.com>
2022-05-17 23:00:08 -0500

    Corrigiendo con ESlint

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,8 +1,8 @@
-const StudentController = require('./../../lib/controllers/StudentController')
+const StudentController = require("./../../lib/controllers/StudentController");
 
-describe('Class StudentController',()=> {
-    test('1. Metodo getStudents',()=>{
+describe("Class StudentController",()=> {
+    test("1. Metodo getStudents",()=>{
         const Students = StudentController.getStudents();
         expect(Students).not.toBeUndefined();
-    })
-})
+    });
+});
